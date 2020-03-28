9b697c884d5e75e62073e7fdc80ff5a48112118c
Vladislav Takchuk <mail@tkachuk.su>
2020-03-28 22:19:13 +0300

    Fixed prickly thind adaptive. Fixed top-menu items hover while opening.

--- src/main/java/com/rhinorus/shblues/ShbluesApplication.java
+++ src/main/java/com/rhinorus/shblues/ShbluesApplication.java
@@ -9,5 +9,5 @@ public class ShbluesApplication {
   	public static void main(String[] args) {
 		SpringApplication.run(ShbluesApplication.class, args);
 	}                                   
-  
+     
 }
