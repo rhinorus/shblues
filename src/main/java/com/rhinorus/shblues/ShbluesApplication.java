dd2f18ec9b9332e715070d3f1f74459f20e99ad3
Vladislav Takchuk <mail@tkachuk.su>
2020-03-28 21:51:27 +0300

    Openable menu done. Videos block started.

--- src/main/java/com/rhinorus/shblues/ShbluesApplication.java
+++ src/main/java/com/rhinorus/shblues/ShbluesApplication.java
@@ -2,12 +2,12 @@ package com.rhinorus.shblues;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class ShbluesApplication {
-
-	public static void main(String[] args) {
+                                       
+  	public static void main(String[] args) {
 		SpringApplication.run(ShbluesApplication.class, args);
-	}   
-
+	}                                   
+  
 }
--- src/main/resources/static/controllers/main.controller.js
+++ src/main/resources/static/controllers/main.controller.js
@@ -1,7 +1,10 @@
 angular
     .module("shblues")
-    .controller("mainController", function MenuController($scope, $http) {
+    .controller("mainController", function MenuController($scope, $http, $window, $timeout) {
  
+        $scope.showMenu = false
+        $scope.showBigMenu = false;
+
         $scope.menu = [
             {
                 name: 'Видео',
@@ -29,9 +32,32 @@ angular
                 anchorId: '#books'
             } 
         ];
+ 
+        $window.onscroll = function(){
+            $timeout(function(){
+                if($window.scrollY > 900 && !$scope.showMenu){
+                    $scope.showMenu = true;
+                }
+                else if($window.scrollY <= 900 && $scope.showMenu){
+                    $scope.showMenu = false;
+                    $scope.showBigMenu = false;
+                } 
+            }, 0);
+        };
+   
+        $scope.scrollToAnchor = function(anchorId, closeTopMenu){
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(anchorId).offset().top  
+            }, 2000); 
 
-        $scope.scrollToAnchor = function(anchorId){
-            alert('Scrolling to ' + anchorId);
+            if(closeTopMenu == true)
+                $scope.showBigMenu = false;
+        };
+
+        $scope.toggleShowBigMenu = function(){
+            $timeout(function(){
+                $scope.showBigMenu = !$scope.showBigMenu;
+            }, 0);
         };
 
     });
