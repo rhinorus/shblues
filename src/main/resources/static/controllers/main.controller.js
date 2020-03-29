166d5ee0e24130589310ba5fc9d1ce6df56e16c8
Vladislav Takchuk <mail@tkachuk.su>
2020-03-29 14:23:49 +0300

    Videos block done.

--- src/main/resources/static/controllers/main.controller.js
+++ src/main/resources/static/controllers/main.controller.js
@@ -32,6 +32,87 @@ angular
                 anchorId: '#books'
             } 
         ];
+
+        $scope.videos = [
+            {
+                name: 'Не орёл',
+                url: 'https://www.youtube.com/watch?v=AtdWfLLSBtI'
+            },
+            {
+                name: 'Blue bird',
+                url: 'https://www.youtube.com/watch?v=OFt_4o6dqGk'
+            },
+            {
+                name: 'Fumo di mentolo',
+                url: 'https://www.youtube.com/watch?v=YtvILC9r14E'
+            },
+            {
+                name: 'КУРИТЬ',
+                url: 'https://www.youtube.com/watch?v=hydZYpXTnrE'
+            }, //4
+            {
+                name: 'TRAIN',
+                url: 'https://www.youtube.com/watch?v=2KYLw194c9w'
+            },
+            {
+                name: 'ПРО КОНЯ',
+                url: 'https://www.youtube.com/watch?v=3C6gljuFs6c'
+            },
+            {
+                name: 'GodKnows',
+                url: 'https://www.youtube.com/watch?v=baQUqhkI5vk'
+            },
+            {
+                name: 'ADULT CONTENT',
+                url: 'https://www.youtube.com/watch?v=0QxkSGkid54'
+            }, //8
+            {
+                name: 'HORSE APOCALIPTIC 2018',
+                url: 'https://www.youtube.com/watch?v=YuUAQa274Jw'
+            }, 
+            {
+                name: 'ТРИ РАЗГОВОРА',
+                url: 'https://www.youtube.com/watch?v=ZCnNNTn9ljY'
+            },
+            {
+                name: 'Ты узнаешь меня',
+                url: 'https://www.youtube.com/watch?v=WmLktht66T8'
+            },
+            {
+                name: 'ХаZZбулат',
+                url: 'https://www.youtube.com/watch?v=qxfYx6rHyXI'
+            }, //12
+            {
+                name: '2 танцора',
+                url: 'https://www.youtube.com/watch?v=xn8t4R-QWWU'
+            },
+            {
+                name: 'MOM',
+                url: 'https://www.youtube.com/watch?v=8I2NzV3jXeA'
+            },
+            {
+                name: 'Че',
+                url: 'https://www.youtube.com/watch?v=Xi26azONVUc'
+            },
+            {
+                name: 'Love for Sale',
+                url: 'https://www.youtube.com/watch?v=v72s4UyNoDs'
+            },
+
+        ]; 
+  
+        $scope.processVideos = function(){
+            for(var video of $scope.videos){
+                var id = video.url.split("?v=")[1];
+                if(id.includes('&'))
+                    id = id.split('&')[0];
+                video.id = id;
+                video.src = "http://i3.ytimg.com/vi/" + id + "/hqdefault.jpg";
+            }
+            
+        }
+
+        $scope.processVideos();
  
         $window.onscroll = function(){
             $timeout(function(){
@@ -59,5 +140,4 @@ angular
                 $scope.showBigMenu = !$scope.showBigMenu;
             }, 0);
         };
-
     });
