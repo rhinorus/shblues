8a83181caec5955f076e891766060441c9d39976
Vladislav Tkachuk <43683662+rhinorus@users.noreply.github.com>
2020-03-31 18:26:39 +0300

    Melodclamations done.

--- src/main/java/com/rhinorus/shblues/ShbluesApplication.java
+++ src/main/java/com/rhinorus/shblues/ShbluesApplication.java
@@ -8,6 +8,6 @@ public class ShbluesApplication {
                                            
   	public static void main(String[] args) {
 		SpringApplication.run(ShbluesApplication.class, args);
-	}                                                                                                                                                                     
+	}                                                                                                                                                                                        
 }                         
-                                                                 
+                                                                      
